3471954eeba2fda9b65d174a831aa2923dd5310f
Kirill Kalachev <kirill.kalachev@neonstingray.com>
2014-03-11 11:42:23 +0400

    Basic working example of the chat

--- ChatApp/Controllers/HomeController.cs
+++ ChatApp/Controllers/HomeController.cs
@@ -8,22 +8,8 @@ namespace ChatApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Chat()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
